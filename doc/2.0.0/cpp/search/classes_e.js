67a953bf5e40b97b9b9b5b5ba9e4ae511933af26
Aravind Raveendran <aravind.raveendran@dolby.com>
2024-07-26 16:34:24 +1000

    Updating 2.0.0 SDK documentation

---
+++ doc/2.0.0/cpp/search/classes_e.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['receivedrtpstream_0',['ReceivedRtpStream',['../structmillicast_1_1rtcstats_1_1_received_rtp_stream.html',1,'millicast::rtcstats']]],
+  ['remoteinboundrtpstream_1',['RemoteInboundRtpStream',['../structmillicast_1_1rtcstats_1_1_remote_inbound_rtp_stream.html',1,'millicast::rtcstats']]],
+  ['remoteoutboundrtpstream_2',['RemoteOutboundRtpStream',['../structmillicast_1_1rtcstats_1_1_remote_outbound_rtp_stream.html',1,'millicast::rtcstats']]],
+  ['renderer_3',['Renderer',['../classmillicast_1_1_renderer.html',1,'millicast']]],
+  ['resolution_4',['Resolution',['../structmillicast_1_1_rts_remote_track_1_1_layers_1_1_resolution.html',1,'millicast::RtsRemoteTrack::Layers']]],
+  ['rtpstream_5',['RtpStream',['../structmillicast_1_1rtcstats_1_1_rtp_stream.html',1,'millicast::rtcstats']]],
+  ['rtsremoteaudiotrack_6',['RtsRemoteAudioTrack',['../classmillicast_1_1_rts_remote_audio_track.html',1,'millicast']]],
+  ['rtsremotetrack_7',['RtsRemoteTrack',['../classmillicast_1_1_rts_remote_track.html',1,'millicast']]],
+  ['rtsremotevideotrack_8',['RtsRemoteVideoTrack',['../classmillicast_1_1_rts_remote_video_track.html',1,'millicast']]],
+  ['rtstrackadded_9',['RtsTrackAdded',['../structmillicast_1_1_viewer_1_1_rts_track_added.html',1,'millicast::Viewer']]]
+];
---
+++ doc/2.0.0/cpp/search/variables_14.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['unique_5fid_0',['unique_id',['../structmillicast_1_1_source_1_1_source_information.html#a3d96d8449ec532d9cf04c8654314298b',1,'millicast::Source::SourceInformation']]],
+  ['uuid_1',['uuid',['../structmillicast_1_1_stream_publish_details.html#abb25c03d85820fc081929e93f8ba7c28',1,'millicast::StreamPublishDetails']]],
+  ['uuid_5fsize_2',['UUID_SIZE',['../classmillicast_1_1_sei_user_unregistered_data.html#a3c257b19c40de794e63e9f0763a6fc8b',1,'millicast::SeiUserUnregisteredData']]]
+];
